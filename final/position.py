951c0ede5907d626bbe9c56d7609ddaaa8d30294
zevbo <u_355322_g_92142@holy2a01202.rc.fas.harvard.edu>
2021-11-22 00:06:34 +0000

    fixing position, working with gazebo now (if calc_tsb is correct)

--- final/position.py
+++ final/position.py
@@ -6,25 +6,16 @@ from sensor_msgs.msg import JointState
 from fk import calc_tsb
 
 def curr_data():
-
-    data = [None]
-
-    def set_data(data):
-        data[0] = data
-
+    
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber('/joint_states', JointState, set_data)
-
-    rospy.spin()
-
-    return data[0]
+    return rospy.wait_for_message('/joint_states', JointState)
 
 def curr_thetas():
-    return curr_data().position
+    return list(curr_data().position)
 
 def curr_v():
     return curr_data().velocity
 
 def curr_t():
-    raise calc_tsb(curr_thetas())
+    return calc_tsb(curr_thetas())
