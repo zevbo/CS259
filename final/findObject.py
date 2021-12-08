0c6bac9e10ff477e8846a02669970ae47264ac87
zevbo <zevminskyprimus@gmail.com>
2021-12-07 22:27:34 -0500

    updates

--- final/findObject.py
+++ final/findObject.py
@@ -33,17 +33,20 @@ def find_centroid(mask):
     return (cx - maxX, cy - maxY), big_contour
 
 
+lower_red = np.array([0, 100, 150])
+upper_red = np.array([10, 255, 255])
+
+lower_green = np.array([170, 50, 50])
+upper_green = np.array([180, 255, 255])
+
+
 def find_obj(img):
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # lower mask (0-10)
-    lower_red = np.array([0, 100, 200])
-    upper_red = np.array([10, 255, 255])
     mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
 
     # upper mask (170-180)
-    lower_red = np.array([170, 50, 50])
-    upper_red = np.array([180, 255, 255])
-    mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+    mask1 = cv2.inRange(img_hsv, lower_green, upper_green)
 
     return find_centroid(mask0 + mask1)
 
