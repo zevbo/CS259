a48ed5d056b1729d71dfb460bbf394344a771226
zevbo <zevminskyprimus@gmail.com>
2021-12-08 00:12:52 -0500

    more adjustment

--- final/triangulization.py
+++ final/triangulization.py
@@ -57,4 +57,5 @@ def triangulate(t_ab, meas_p_a, z1):
     sol, p = get_solution_simple(t_ab, meas_p_a, z1)
     real_p = list(map(lambda el: 1 if el ==
                   1 else el[0].evalf(subs={z1: sol[0]}), p))
+    real_p[1] -= 15
     return real_p
