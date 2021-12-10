9ec0c50bbf9d12dd11e5463961cac4b2e11b9770
zevbo <zevminskyprimus@gmail.com>
2021-12-10 13:45:41 -0500

    upadte

--- final/camera.py
+++ final/camera.py
@@ -23,5 +23,5 @@ maxY = 240
 # this is a guess rn
 angle = 30 * math.pi / 180
 r_bc = y_rotation(angle)
-p_bc = np.array([40, 0, -152])
+p_bc = np.array([13.5, 0, -152])
 t_bc = r_and_shift_to_t(r_bc, p_bc)
