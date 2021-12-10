1dc1fdac6a4cb3dec654685ad08f60baac1ba629
zevbo <zevminskyprimus@gmail.com>
2021-12-10 12:41:11 -0500

    changed triangulization

--- final/triangulization.py
+++ final/triangulization.py
@@ -6,39 +6,31 @@ from sympy import symbols, solve, Eq, linsolve, core
 import numpy as np
 
 
+def value_of(exp, v1, v2):
+    return exp.evalf(subs={v1: 1.0, v2: 0.0}) - exp.evalf(subs={v1: 0.0, v2: 0.0})
+
+
+def row(zero, z1, z2):
+    return [value_of(zero, z1, z2), value_of(zero, z2, z1)]
+
+
+def constant_of(exp, v1, v2):
+    return - exp.evalf(subs={v1: 0.0, v2: 0.0})
+
+
 def get_solution(t_ab, meas_p_a, meas_p_b, z1, z2):
 
     meas_p_a2 = np.dot(t_ab, meas_p_b)
     zero_p = [a[0] for a in (meas_p_a2 - meas_p_a).tolist()[:-1]]
 
-    sol = linsolve(zero_p, (z1, z2))
+    m = np.array([row(zero_p[0], z1, z2), row(
+        zero_p[1], z1, z2), row(zero_p[2], z1, z2)])
+    y = np.transpose(np.array(
+        [[constant_of(zero_p[0]), constant_of(zero_p[1]), constant_of(zero_p[2])]]))
 
-    if sol != EmptySet:
-        return list(sol)[0]
+    x = np.dot(pseduo_inv(m), y)
 
-    sol1 = linsolve([zero_p[0], zero_p[1]], (z1, z2))
-    sol2 = linsolve([zero_p[0], zero_p[2]], (z1, z2))
-    sol3 = linsolve([zero_p[1], zero_p[2]], (z1, z2))
-    solutions = [sol1, sol2, sol3]
-    final_sol_sum = np.array([0.0, 0.0])
-    num_terms = 0
-
-    def is_float(n):
-        return isinstance(n, core.numbers.Float)
-
-    for sol in solutions:
-
-        if len(sol) == 1:
-            spec_sol = np.array(list(sol)[0])
-            if is_float(spec_sol[0]) and is_float(spec_sol[1]):
-                spec_sol = np.array(list(map(float, spec_sol)))
-                final_sol_sum += spec_sol
-                num_terms += 1
-
-    if num_terms == 0:
-        raise RuntimeError("get_solution found no solution!")
-
-    return final_sol_sum / num_terms
+    return x[0]
 
 # returns real p_a
 
